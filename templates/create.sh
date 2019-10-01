49478e564f8eaa815f8dc034db16ba671fbb7c4d
mcampora <mcampora@gmail.com>
2019-10-01 13:34:25 +0200

    ...

--- templates/create.sh
+++ templates/create.sh
@@ -1,15 +1,19 @@
+# create a temporary bucket with static content
+aws s3 mb s3://skiagenda-source --region us-east-1
+
+# upload static files
+aws s3 sync ../website s3://skiagenda-source/website
+aws s3 sync ../backend s3://skiagenda-source/backend
+
 # create the website resources
 aws cloudformation create-stack \
     --capabilities CAPABILITY_IAM \
     --stack-name skiagenda-website \
-    --template-body file://website.template 
+    --template-body file://website.yaml 
 
 # wait for completion
 aws cloudformation wait stack-create-complete \
     --stack-name skiagenda-website
-
-# upload static files
-aws s3 sync ../website/ s3://skiagenda
 
 # create the backend resources
 aws cloudformation create-stack \
