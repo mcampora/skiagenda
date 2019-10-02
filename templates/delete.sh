2047834180c173c2e9b1ae13bd0bf079edb594be
mcampora <mcampora@gmail.com>
2019-10-03 00:21:42 +0200

    x

--- templates/delete.sh
+++ templates/delete.sh
@@ -1,8 +1,9 @@
+# cleanup the stacks
+aws cloudformation delete-stack --stack-name skiagenda-website
+
 # empty the website bucket
 aws s3 rb s3://skiagenda --force
 
 # cleanup the temporary S3 bucket
 aws s3 rb s3://skiagenda-source --force
 
-# cleanup the stacks
-aws cloudformation delete-stack --stack-name skiagenda-website
