31b193b3cb83031e7cdec586fd332d3c806625e4
mcampora <mcampora@gmail.com>
2019-10-11 21:32:24 +0200

    almost there, config update is ok, still some issues with CORS

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 var _config = {
     cognito: {
-        userPoolId: 'us-east-1_qbMPGpf4K', 
-        userPoolClientId: 'makn6vbbm9hn34na3s55ua2ep', 
-        region: 'us-east-1', // e.g. us-east-2
+        userPoolId: '', 
+        userPoolClientId: '', 
+        region: ''
     },
     api: {
-        invokeUrl: 'https://q7s84l6o8g.execute-api.us-east-1.amazonaws.com/prod'
+        invokeUrl: ''
     }
 };
